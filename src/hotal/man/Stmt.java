3e2b188fc034779756bbec1aa71b95ba03610414
Sandeep Kumar <sandeepkumar@amceducation.in>
2023-04-21 09:09:19 +0530

    Initial Commit

---
+++ src/hotal/man/Stmt.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hotal.man;
+
+/**
+ *
+ * @author HCL
+ */
+class Stmt {
+
+    static void executeUpdate(String Query) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
